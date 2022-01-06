9960df54bc24206340e5d304155c69142232176a
Amit-R-Bhagat <amit.bhagat3301@gmail.com>
2022-01-06 19:19:34 +0530

    used to extract college course list

--- web-scraper/index.js
+++ web-scraper/index.js
@@ -1,24 +1,33 @@
+const fs = require("fs");
 const rp = require("request-promise");
 const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
-const url = "https://www.reddit.com/";
+const url =
+  "https://docs.google.com/forms/d/e/1FAIpQLSeHfXr-SAgfRSGIXC7WN_KyD8afGDUeR6xWKm65orp-NXljsw/viewform";
 // https://www.reddit.com/
 // https://archivepmo.nic.in/
 
 // rp(url).then((webpage) => {
 //   console.log(webpage);
 // });
-
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  await page.screenshot({ path: "example.png" });
-  const html = await page.content();
-  const $ = cheerio.load(html);
-  $("h3").each(function () {
-    console.log($(this).text());
-  });
+  try {
+    console.log("hello world");
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.screenshot({ path: "example.png" });
+    const html = await page.content();
+    const $ = cheerio.load(html);
+    $(".freebirdFormviewerComponentsQuestionBaseTitle").each(function () {
+      let data = $(this).text();
+      console.log(data);
+      fs.unlinkSync("data.txt");
+      fs.appendFileSync("data.txt", `${data.replace("*", "")}\n`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 })();
 
 // rp(url)
