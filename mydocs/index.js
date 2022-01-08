23ae3e4a5f9142fcc8aceba31462c086c1b832ca
Amit-R-Bhagat <amit.bhagat3301@gmail.com>
2022-01-08 21:06:35 +0530

    swagger docs

---
+++ mydocs/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/", (req, res) => {
+  res.status(200).send("hello");
+});
+
+app.listen(3000, () => console.log("server started on port 3000"));
